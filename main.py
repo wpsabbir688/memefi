fe4615315b6b89264573dd322ed5492f09f10193
wpsabbir688 <wpsabbir688@gmail.com>
2024-10-18 21:06:07 +0600

    first commit

---
+++ main.py
@@ -0,0 +1,58 @@
+import os
+import subprocess
+
+# Define ANSI escape codes for colors
+RESET = "\033[0m"
+RED = "\033[31m"
+GREEN = "\033[32m"
+YELLOW = "\033[33m"
+BLUE = "\033[34m"
+MAGENTA = "\033[35m"
+CYAN = "\033[36m"
+WHITE = "\033[37m"
+
+# Colorful banner
+banner = f"""{GREEN}
+  __  __       _____ _               _    
+ |  \/  |     / ____| |             | |   
+ | \  / |_ __| |  __| |__   ___  ___| |_  
+ | |\/| | '__| | |_ | '_ \ / _ \/ __| __| 
+ | |  | | |_ | |__| | | | | (_) \__ \ |_  
+ |_|  |_|_(_) \_____|_| |_|\___/|___/\__| 
+                                          
+{RESET}"""
+
+# Colorful info
+info = f"""{YELLOW}
+--------------------------------------------------------
+Author : {CYAN}MrGhostXBOT{YELLOW}
+Github : {CYAN}@MrGhost69{YELLOW}
+Telegram : {CYAN}@CSFTEAM3{YELLOW}
+TOOLS : {CYAN}MEMEFI COIN UP{YELLOW}
+--------------------------------------------------------{RESET}
+"""
+
+os.system("clear")
+print(banner)
+print(info)
+
+auth_token = input(f"{MAGENTA}Enter your auth token: {RESET}")
+
+# Step 2: Delete the old query_id.txt file if it exists
+file_path = 'query_id.txt'
+if os.path.exists(file_path):
+    os.remove(file_path)
+    print(f"{GREEN}----------------------------------------------------------{RESET}")
+else:
+    print(f"{YELLOW}does not exist, creating a new one.{RESET}")
+
+# Step 3: Save the new auth token into query_id.txt
+with open(file_path, 'w') as file:
+    file.write(auth_token)
+    print(f"{GREEN}----------------------------------------------------------{RESET}")
+
+# Step 4: Run memefi.py
+try:
+    subprocess.run(['python', 'memefi.py'])
+except Exception as e:
+    print(f"{RED}Failed to run memefi.py: {e}{RESET}")
